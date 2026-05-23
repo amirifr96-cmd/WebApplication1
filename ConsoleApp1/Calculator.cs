8000dad725fcfe191b494184ef1e6c4dc3607ae8
amiri <amiri.f.r96@gmail.com>
2026-05-23 08:59:54 -0700

    multi

--- ConsoleApp1/Calculator.cs
+++ ConsoleApp1/Calculator.cs
@@ -7,10 +7,14 @@ using System.Threading.Tasks;
 namespace ConsoleApp1
 {
    public class Calculator
-    {
+   {
         public int sum(int a,int b)
         {
             return a + b;
         }
-    }
+        public int multi(int a,int b)
+        {
+            return a * b;
+        }
+   }
 }
