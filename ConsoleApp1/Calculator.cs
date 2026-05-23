02eaae38ea5c4bd807177b82f445c4540409c203
amiri <amiri.f.r96@gmail.com>
2026-05-23 08:37:42 -0700

    cal

---
+++ ConsoleApp1/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public class Calculator
+    {
+        public int sum(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("A!");
-Console.WriteLine("B!");
+using ConsoleApp1;
+Calculator cal = new Calculator();
+Console.WriteLine("please inter num1");
+
+
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("please inter num2");
+int num2=int.Parse(Console.ReadLine());
+ int  result=cal.sum(num1, num2);
+Console.WriteLine($" sum :{result}");
+Console.ReadKey();
 
