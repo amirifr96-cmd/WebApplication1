e1649042c72acad50ff420013d490a986176fb1c
amiri <amiri.f.r96@gmail.com>
2026-05-23 03:28:58 -0700

    TWO TAGHIR

---
+++ WebApplication1/Controllers/ME.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ME : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GETlIST()
+        {
+            return View();
+        }
+    }
+}
